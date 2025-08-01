591407a94badb91485231573c87ba0bb25eda045
Charlie Flux <mail@charlieflux.com>
2025-08-01 22:03:53 +0100

    Rearranged parameters

--- rtl/parameters.sv
+++ rtl/parameters.sv
@@ -1,8 +1,9 @@
 // ALU Operation Codes
-`define ALU_ADD      4'b0010
-`define ALU_SUB      4'b0110
 `define ALU_AND      4'b0000
 `define ALU_OR       4'b0001
+`define ALU_ADD      4'b0010
+`define ALU_SUB      4'b0110
+`define ALU_XOR      4'b0110
 `define ALU_INVALID  4'bxxxx
 
 // ALUOP signals
